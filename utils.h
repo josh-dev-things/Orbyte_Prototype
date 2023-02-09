f033bfe9c6f98fddb3e231f6642c31392decfd8f
EthanKesslerDev <ethankessler.dev@gmail.com>
2023-02-09 10:04:35 +0000

    This isnt going to work. You need to make a hash table and look into what data is necessary... good luck this is going to be a slog.

--- utils.h
+++ utils.h
@@ -6,23 +6,33 @@
 #include "OrbitBody.h"
 #include <vector>
 
-std::vector<Body> MergeOrbitBodies(std::vector<Body> left, std::vector<Body> right)
+std::vector<Body> MergeOrbitBodies(std::vector<Body>& left, std::vector<Body>& right)
 {
-	std::vector<Body> Merged;
+	std::vector<std::string> Merged(left.size() + right.size());
 
-	int iLeft = 0; // left current position
-	int iRight = 0; // right current position
-	int iMerged = 0; // merged current position
+	int iLeft = 0; // left index
+	int iRight = 0; // right index
+	int iMerged = 0; // merged index
 
 	while (iLeft < left.size() && iRight < right.size())
 	{
 		if (Magnitude(left[iLeft].Get_Position()) < Magnitude(right[iRight].Get_Position()))
 		{
 		//https://github.com/isaaccomputerscience/isaac-code-samples/blob/main/sorting-algorithms/merge-sort/c-sharp/merge_sort.cs
+			Merged[iMerged] = left[iLeft].name;
+			iLeft++;
+			iMerged++;
+		}
+		else {
+			Merged[iMerged] = right[iRight].name;
+			iRight++;
+			iMerged++;
 		}
 	}
 
+	//Clean up any data not included
 
+	while (iLeft < left.size())
 
 }
 
