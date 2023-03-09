e7863929d1f67538ba1923b2673c128ce53b3ca8
EthanKesslerDev <ethankessler.dev@gmail.com>
2023-03-09 19:33:53 +0000

    more testing

--- OrbitBody.h
+++ OrbitBody.h
@@ -591,7 +591,7 @@ public:
 	void RecenterBody()
 	{
 		MoveToPos(position);
-		Project_Circular_Orbit(velocity);
+		//Project_Circular_Orbit(velocity);
 		start_pos = position;
 		time_since_start = 0;
 	}
